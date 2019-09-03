98153c77239d959195f7ef4034d57fd688fd974b
“Wade <hello@wade-jensen.com>
2019-09-03 12:27:40 +1000

    add type declarations for bundle report structure

---
+++ src/index.d.ts
@@ -0,0 +1,25 @@
+export declare type BundleReport = JsBundle[];
+export declare type JsBundle = {
+    label: string;
+    statSize: number;
+    parsedSize: number;
+    gzipSize: number;
+    groups: FileGroup[];
+};
+export declare type Directory = {
+    label: string;
+    path: string;
+    statSize: number;
+    parsedSize: number;
+    gzipSize: number;
+    groups: FileGroup[];
+};
+export declare type File = {
+    id: string;
+    label: string;
+    path: string;
+    statSize: number;
+    parsedSize: number;
+    gzipSize: number;
+};
+export declare type FileGroup = File | Directory;
